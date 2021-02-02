115f36f93d0c2c39a74c3017668ef917ff4b96f6
Cunliang Geng <c.geng@esciencecenter.nl>
2021-02-02 13:33:02 +0100

    Remove NullHandler from top logger

--- fdpclient/__init__.py
+++ fdpclient/__init__.py
@@ -4,7 +4,7 @@ from .__version__ import __version__
 from .config import DATA_FORMATS
 from .operations import create, read, update, delete
 
-logging.getLogger(__name__).addHandler(logging.NullHandler())
+logging.getLogger(__name__)
 
 __author__ = "Cunliang Geng"
 __email__ = 'c.geng@esciencecenter.nl'
