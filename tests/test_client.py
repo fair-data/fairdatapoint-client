25d093544cc859236fa94b21bd71ed90f7107973
Cunliang Geng <c.geng@esciencecenter.nl>
2021-03-23 10:39:22 +0100

    Add tests for fdp methods

--- tests/test_client.py
+++ tests/test_client.py
@@ -4,12 +4,10 @@ import requests
 
 from fdpclient.client import Client
 
-# TODO
-# - add tests on fdp
-
 base_url = 'http://example.org'
 catalogID = 'catalog01'
 catalog_url = base_url + '/catalog'
+fdp_url = base_url + '/fdp'
 data_url = catalog_url + '/' + catalogID
 
 # datadir fixture provided via pytest-datadir-ng
@@ -24,14 +22,25 @@ def data_update(datadir):
         return f.read()
 
 @pytest.fixture()
+def data_fdp(datadir):
+    with open(datadir['fdp.ttl']) as f:
+        return f.read()
+
+@pytest.fixture()
+def data_fdp_update(datadir):
+    with open(datadir['fdp_update.ttl']) as f:
+        return f.read()
+
+@pytest.fixture()
 def client(requests_mock):
-    requests_mock.get(base_url+'/fdp', status_code=200, headers={'content-type': 'text/turtle'})
+    requests_mock.get(base_url + '/fdp', status_code=200, headers={'content-type': 'text/turtle'})
     return Client(base_url)
 
 class TestDefault:
     """Test fdpclient.client.Client methods"""
 
     # requests_mock fixture provided via requests-mock
+    # test catalog
     def test_create_catalog(self, client, data, requests_mock):
         """Test create_catalog method"""
         requests_mock.post(catalog_url)
@@ -55,4 +64,24 @@ class TestDefault:
         """Test delete_catalog method"""
         requests_mock.delete(data_url)
         r = client.delete_catalog(catalogID)
+        assert r is None
+
+    # test fdp
+    def test_create_fdp(self, client, data_fdp, requests_mock):
+        """Test create_fdp method"""
+        requests_mock.post(fdp_url)
+        r = client.create_fdp(data=data_fdp)
+        assert r is None
+
+    def test_read_fdp(self, client, data_fdp, requests_mock):
+        """Test read_fdp method"""
+        requests_mock.get(fdp_url, text=data_fdp)
+        r = client.read_fdp()
+        assert isinstance(r, rdflib.Graph)
+        assert  b'hasVersion "1.0"' in r.serialize(format='turtle')
+
+    def test_update_fdp(self, client, data_fdp_update, requests_mock):
+        """Test update_fdp method"""
+        requests_mock.put(fdp_url, text=data_fdp_update)
+        r = client.update_fdp(data=data_fdp_update)
         assert r is None
